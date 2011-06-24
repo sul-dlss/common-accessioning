33f211ca432394d8b843141bbecc7a3aac11718b
Michael Klein <mbklein@gmail.com>
2011-06-24 10:24:47 -0700

    Subclass service controller directly from Daemons::ApplicationGroup

--- lib/robot_service_controller.rb
+++ lib/robot_service_controller.rb
@@ -3,97 +3,111 @@ require 'logger'
 
 module LyberCore
   module Robots
-    class ServiceController
+    class ServiceController < Daemons::ApplicationGroup
       attr_reader :logger, :pid_dir
 
-      def initialize(workflow, opts = {})
+      def initialize(opts = {})
         @logger = opts[:logger] || Logger.new($stdout)
         @logger.level = opts[:log_level] || Logger::ERROR
-        @pid_dir = opts[:pid_dir] || File.join(File.dirname(__FILE__), '../pid')
+        @working_dir = opts[:working_dir] || ENV['ROBOT_ROOT'] || Dir.pwd
+        @pid_dir = opts[:pid_dir] || File.join(@working_dir, '../pid')
         @pid_dir = File.expand_path(pid_dir)
-        @workflow = workflow
-        @logger.debug "Initializing application group for #{@workflow}."
+        @logger.debug "Initializing application group."
         @logger.debug "Writing pids to #{@pid_dir}"
-        @group = Daemons::ApplicationGroup.new(workflow, :dir_mode => :normal, :dir => @pid_dir, :multiple => true)
-        @yaml_file = File.join(@pid_dir, "#{@workflow}.yml")
+        super('robot_service_controller', :dir_mode => :normal, :dir => @pid_dir, :multiple => true)
       end
 
-      def start(robot_name, &block)
-        with_robot_info do
-          result = false
-          app = find_app(robot_name)
-          if app.nil? or (app.running? == false)
-            @logger.info "Starting #{@workflow}:#{robot_name}..."
+      def start(workflow, robot_name)
+        result = false
+        app = find_app(workflow, robot_name).first
+        if app.nil? or (app.running? == false)
+          @logger.info "Starting #{workflow}:#{robot_name}..."
+          with_app_name("#{workflow}:#{robot_name}") do
             app, message = capture_stdout do
-              new_app = @group.new_application({:mode => :proc, :proc => block})
+              module_name = workflow.split('WF').first.capitalize
+              robot_klass = Module.const_get(module_name).const_get(robot_name.split(/-/).collect { |w| w.capitalize }.join(''))
+              robot = robot_klass.new
+              robot_proc = lambda {
+                Dir.chdir(@working_dir) do
+                  loop { sleep(15*60) unless robot.start }
+                end
+              }
+              new_app = self.new_application({:mode => :proc, :proc => robot_proc})
               new_app.start
               new_app
             end
-            if app.running?
-              @logger.info "#{@workflow}:#{robot_name} [#{app.pid.pid}] started."
-              @robots[robot_name] = app.pid.pid
-              result = true
-            else
-              @logger.error "Unable to start #{@workflow}:#{robot_name}"
-              @robots.delete(:robot_name)
-            end
-          else app.running?
-            @logger.warn "Robot #{@workflow}:#{robot_name} [#{app.pid.pid}] is already running"
           end
-          result
-        end
-      end
-  
-      def stop(robot_name)
-        with_robot_info do
-          app = find_app(robot_name)
-          result = false
-          if app.nil?
-            @logger.info "Robot #{@workflow}:#{robot_name} not found"
-          elsif app.running?
-            @logger.info "Shutting down #{@workflow}:#{robot_name} [#{app.pid.pid}]..."
-            result, message = capture_stdout { app.stop }
-            if not app.running?
-              @logger.info "#{@workflow}:#{robot_name} [#{app.pid.pid}] shut down."
-              @robots.delete(robot_name)
-              result = true
-            else
-              @logger.error "Unable to stop #{@workflow}:#{robot_name} [#{app.pid.pid}]."
-            end
+          
+          if app.running?
+            @logger.info "#{workflow}:#{robot_name} [#{app.pid.pid}] started."
+            result = true
           else
-            @logger.warn "Robot #{@workflow}:#{robot_name} [#{app.pid.pid}] is not running but pidfile exists"
-            @robots.delete(robot_name)
+            @logger.error "Unable to start #{workflow}:#{robot_name}"
           end
-          result
+        else app.running?
+          @logger.warn "Robot #{workflow}:#{robot_name} [#{app.pid.pid}] is already running"
         end
+        return result
       end
-  
-      def status(robot_name)
-        with_robot_info do
-          app = find_app(robot_name)
-          result = { :status => nil, :pid => nil }
-          unless app.nil?
-            result[:pid] = app.pid.pid
-            result[:status] = app.running? ? :running : :stopped
-          end
-          result
-        end
-      end
-  
-      def status_message(robot_name)
-        app_status = status(robot_name)
-        message = case app_status[:status]
-        when :running
-          "Robot #{@workflow}:#{robot_name} [#{app_status[:pid]}] is running"
-        when :stopped
-          "Robot #{@workflow}:#{robot_name} [#{app_status[:pid]}] is not running but pidfile exists"
+
+      def stop(workflow, robot_name)
+        apps = find_app(workflow, robot_name)
+        result = false
+        if apps.empty?
+          @logger.info "Robot #{workflow}:#{robot_name} not found"
         else
-          "Robot #{@workflow}:#{robot_name} not found"
+          apps.each do |app|
+            if app.running?
+              @logger.info "Shutting down #{workflow}:#{robot_name} [#{app.pid.pid}]..."
+              result, message = capture_stdout { app.stop }
+              if app.running?
+                @logger.error "Unable to stop #{workflow}:#{robot_name} [#{app.pid.pid}]."
+              else
+                @logger.info "#{workflow}:#{robot_name} [#{app.pid.pid}] shut down."
+                result = true
+              end
+            else
+              @logger.warn "Robot #{workflow}:#{robot_name} [#{app.pid.pid}] is not running but pidfile exists"
+              app.zap!
+            end
+          end
         end
-        return message
+        result
+      end
+  
+      def status(workflow, robot_name)
+        apps = find_app(workflow, robot_name)
+        apps.collect do |app|
+          { :pid => app.pid.pid, :status => app.running? ? :running : :stopped }
+        end
+      end
+  
+      def status_message(workflow, robot_name)
+        app_status = status(workflow, robot_name)
+        if app_status.empty?
+          ["Robot #{workflow}:#{robot_name} not found"]
+        else
+          app_status.collect do |s|
+            case s[:status]
+            when :running
+              "Robot #{workflow}:#{robot_name} [#{s[:pid]}] is running"
+            when :stopped
+              "Robot #{workflow}:#{robot_name} [#{s[:pid]}] is not running but pidfile exists"
+            end
+          end
+        end
       end
   
       private
+      def with_app_name(name)
+        old_name, @app_name = @app_name, name
+        begin
+          return yield
+        ensure
+          @app_name = old_name
+        end
+      end
+      
       def capture_stdout
         old_io = $stdout
         begin
@@ -107,34 +121,10 @@ module LyberCore
         end
       end
   
-      def with_robot_info
-        result = begin
-          load_robot_info
-          yield
-        ensure
-          save_robot_info
-        end
-        return result
+      def find_app(workflow, robot_name)
+        self.find_applications_by_app_name("#{workflow}:#{robot_name}")
       end
   
-      def find_app(robot_name)
-        with_robot_info { @group.find_applications_by_app_name(@workflow).find { |a| a.pid.pid == @robots[robot_name] } }
-      end
-  
-      def load_robot_info
-        if File.exists?(@yaml_file)
-          @robots = YAML.load(File.read(@yaml_file))
-          @logger.debug "Loaded pids #{@robots.inspect} from #{@yaml_file}"
-        else
-          @logger.debug "Creating new pid hash"
-          @robots = {}
-        end
-      end
-  
-      def save_robot_info
-        @logger.debug "Saving pids #{@robots.inspect} to #{@yaml_file}"
-        File.open(@yaml_file, 'w') { |f| YAML.dump(@robots, f) }
-      end
     end
   end
 end
