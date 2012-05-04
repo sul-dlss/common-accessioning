8081d99a4f1e7677b3950aaee28df68b6a38ed38
Willy Mene <wmene@stanford.edu>
2012-05-04 16:11:47 -0700

    Disable republisher and embargo-release during remediation

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,6 @@
 set :output, '/home/lyberadmin/common-accessioning/current/log/crondebug.log'
 
-every :day, :at => '2:16am' do
-  command "ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/ruby-1.8.7-p358\\@common-accessioning/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
-end
+# !!!!!!!!!!!!!!!!!!!!!! Embargo-Release disabled during Remediation!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#every :day, :at => '2:16am' do
+#  command "ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/ruby-1.8.7-p358\\@common-accessioning/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
+#end
