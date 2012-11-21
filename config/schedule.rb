3d2edc172d6bf743877df95532e19afdcd61c76e
Willy Mene <wmene@stanford.edu>
2012-11-20 16:22:04 -0800

    Disable embargo-release when versioning is released

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, '/home/lyberadmin/common-accessioning/current/log/crondebug.log'
 
-every :day, :at => '2:16am' do
- command "BUNDLE_GEMFILE=/home/lyberadmin/common-accessioning/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/ruby-1.8.7-p358/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
-end
+# every :day, :at => '2:16am' do
+#  command "BUNDLE_GEMFILE=/home/lyberadmin/common-accessioning/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/ruby-1.8.7-p358/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
+# end
