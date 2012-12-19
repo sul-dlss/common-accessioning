0954ad4bf8a2ddb09b1d234336a6cfcae820bdb2
Jonathan Deering <jdeerin1@slu.edu>
2012-12-19 14:22:52 -0800

    missing step

--- robots/accession/descriptive_metadata.rb
+++ robots/accession/descriptive_metadata.rb
@@ -4,7 +4,7 @@ module Accession
   
   class DescriptiveMetadata < AbstractMetadata
     def self.params
-      { :process_name => 'descriptive-metadata', :datastream => 'descMetadata' }
+      { :process_name => 'descriptive-metadata', :datastream => 'descMetadata', :require => true }
     end
   end
 end
