fa85622328693382106fe4f0b528f8d62741d1fe
Willy Mene <wmene@stanford.edu>
2014-02-11 16:01:58 -0800

    Tweak shared directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :deploy_to, "/home/lyberadmin/#{fetch(:application)}"
 
 set :stages, %W(dev staging production)
 
-set :linked_dirs, %w(log config/environments config/environments)
+set :linked_dirs, %w(log config/environments config/certs)
 
 namespace :deploy do
 
