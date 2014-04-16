18a9bbd5e8362402a7067f26b40e4f08ac0e41bc
Darren Hardy <drh@stanford.edu>
2014-04-16 12:47:14 -0700

    added restart mechanism for controller -- see #4

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,22 +42,16 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    on roles(:app), in: :sequence, wait: 10 do
+      within release_path do
+        execute :controller, :stop
+        execute :controller, :quit
+        execute :controller, :boot
+      end
     end
   end
 
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 
 end
 
