0ae0c7de52827cfb496f8d96c17a41c977ba8d8a
Willy Mene <wmene@stanford.edu>
2014-05-05 13:33:24 -0700

    Deploy to bots-dev VMs

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,6 +1,5 @@
-server 'sul-lyberservices-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-# server 'sul-robots1-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-# server 'sul-robots2-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots1-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots2-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
