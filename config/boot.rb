f26b2e9e6d0a405e809c6a49f2385f8a57daa952
Darren Hardy <drh@stanford.edu>
2014-07-02 11:16:23 -0700

    move robot inclusion to robots/robots.rb

--- config/boot.rb
+++ config/boot.rb
@@ -24,26 +24,13 @@ require 'lyber_core'
 
 # TODO Maybe move auto-require to just run_robot and spec_helper?
 Dir["#{ROBOT_ROOT}/lib/**/*.rb"].each { |f| require f }
-require 'accession/abstract_metadata'
-require 'accession/content_metadata'
-require 'accession/descriptive_metadata'
-require 'accession/end_accession'
-require 'accession/provenance_metadata'
-require 'accession/publish'
-require 'accession/remediate_object'
-require 'accession/rights_metadata'
-require 'accession/sdr_ingest_transfer'
-require 'accession/shelve'
-require 'accession/technical_metadata'
-require 'dissemination/cleanup'
+require 'robots'
 
 env_file = File.expand_path(File.dirname(__FILE__) + "/./environments/#{environment}")
 puts "Loading config from #{env_file}"
 require env_file
 
 require 'resque'
-REDIS_URL ||= "localhost:6379/resque:#{ENV['ROBOT_ENVIRONMENT']}"
-Resque.redis = REDIS_URL
+Resque.redis = REDIS_URL || "localhost:6379/resque:#{ENV['ROBOT_ENVIRONMENT']}"
 
-require 'active_support/core_ext' # camelcase
 require 'robot-controller'
