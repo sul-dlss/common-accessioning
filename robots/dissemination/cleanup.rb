c27a81f2b2a58828bbe47627cf51c09351405053
Ahmed AlSum <aalsum@stanford.edu>
2014-07-17 12:26:32 -0700

    cleanup robot will call the new CleanupResetService for the reset-druid

--- robots/dissemination/cleanup.rb
+++ robots/dissemination/cleanup.rb
@@ -11,11 +11,21 @@ module Robots
         end
 
         def perform(druid)
-          Dor::CleanupService.cleanup_by_druid druid
+          #decide whether the druid is full or truncated 
+          if is_full_druid_tree(druid)
+            Dor::CleanupService.cleanup_by_druid druid
+          else
+            Dor::CleanupResetService.cleanup_by_reset_druid druid
+          end
         end
-
+        
+        #determines if the druid is the regular druid tree or the truncated one
+        def is_full_druid_tree(druid)
+          full_druid_tree = DruidTools::Druid.new(druid,workspace_root)
+          truncate_druid_tree = DruidTools::AccessDruid.new(druid,workspace_root)
+          return (!Dir.glob(truncate_druid_tree.path).empty? and !Dir.glob(full_druid_tree.path+"*").empty?)
+        end
       end
-
     end
   end
 end
