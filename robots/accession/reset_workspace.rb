545c823efebb13edd4d075f3975107480daacef2
Ahmed AlSum <aalsum@stanford.edu>
2014-07-17 12:24:58 -0700

    Change the name archive-workspace to reset-workspace

--- robots/accession/reset_workspace.rb
+++ robots/accession/reset_workspace.rb
@@ -3,18 +3,22 @@ module Robots
   module DorRepo
     module Accession
 
-      class ArchiveWorkspace
+      class ResetWorkspace
         include LyberCore::Robot
 
         def initialize
-          super('dor', 'accessionWF', 'archive-workspace')
+          super('dor', 'accessionWF', 'reset-workspace')
         end
 
         def perform(druid)
           druid_obj = Dor::find(druid)
           version = druid_obj.current_version
+
           workspace_root = Dor::Config.stacks.local_workspace_root
-          Dor::ArchiveWorkspaceService.archive_workspace_druid_tree(druid, version, workspace_root)
+          export_home = Dor::Config.cleanup.local_export_home
+
+          Dor::ResetWorkspaceService.reset_workspace_druid_tree(druid, version, workspace_root)
+          Dor::ResetWorkspaceService.reset_export_bag(druid, version, export_home)
         end
       end
 
