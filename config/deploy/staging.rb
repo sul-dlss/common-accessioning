ea4f3ec9635584b58665f210c6e23bde70e0aa60
Willy Mene <wmene@stanford.edu>
2014-08-15 17:33:19 -0700

    Set ROBOT_ENVIRONMENT variable for capistrano. Add staging servers

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,11 @@
-server 'sul-lyberservices-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots1-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots2-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots3-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots4-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots5-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :deploy_environment, 'test'
 set :whenever_environment, fetch(:deploy_environment)
+set :default_env, { :robot_environment => fetch(:deploy_environment) }
