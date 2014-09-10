7dba2b1dda41ab59176f3bb0f369adeab6dd3724
Willy Mene <wmene@stanford.edu>
2014-09-10 09:55:57 -0700

    Add prod bot servers to cap config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
-server 'sul-lyberservices-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots1-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots2-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
