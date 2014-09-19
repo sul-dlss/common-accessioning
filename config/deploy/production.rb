cd28546627bc646243371d59505628bfd70673ee
Willy Mene <wmene@stanford.edu>
2014-09-19 16:05:07 -0700

    Only run capistrano whenever tasks on certain servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server 'sul-robots1-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots2-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-robots2-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
