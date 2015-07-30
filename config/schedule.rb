1f7fb83d9ae95ba8b161aa37cb578f03cb3aa0f5
Rosalyn Metz <rosalynmetz@gmail.com>
2015-07-30 09:38:35 -0700

    update embargo release cron
    we only want the embargo release cron to run on one machine, not all of them.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, '/home/lyberadmin/common-accessioning/current/log/crondebug.log'
 
-every :day, :at => '2:16am', :roles => [:app] do
+every :day, :at => '2:16am', :roles => [:db] do
  command "BUNDLE_GEMFILE=/home/lyberadmin/common-accessioning/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/default/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
 end
 
