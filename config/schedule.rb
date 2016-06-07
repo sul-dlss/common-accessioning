de97e320000ede6034247592d8dce739df487093
Tommy Ingulfsen <tommyi@stanford.edu>
2016-06-07 11:29:54 -0700

    Run cron job from within correct directory.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, '/home/lyberadmin/common-accessioning/current/log/crondebug.log'
 
 every :day, :at => '2:16am', :roles => [:db] do
- command "BUNDLE_GEMFILE=/home/lyberadmin/common-accessioning/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/default/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
+ command "cd /home/lyberadmin/common-accessioning/current/ && BUNDLE_GEMFILE=/home/lyberadmin/common-accessioning/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/default/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
 end
 
 every 5.minutes, :roles => [:app] do
