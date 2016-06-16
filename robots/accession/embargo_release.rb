7f237d926c4c435cd6e6a9cfaf0dd07077b35d2a
Naomi Dushay <ndushay@stanford.edu>
2016-06-15 17:24:29 -0700

    embargo_release: make it a class and write tests

--- robots/accession/embargo_release.rb
+++ robots/accession/embargo_release.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../config/boot')
+
 #!/usr/bin/env ruby
 #
 # Finds objects where the embargo release date has passed for embargoed items
@@ -6,62 +8,62 @@
 # Should run once a day from cron
 # Example cron entry
 # 16 2 * * * bash --login -c 'cd /home/lyberadmin/common-accessioning && ROBOT_ENVIRONMENT=test ruby ./robots/accession/embargo_release.rb' > /home/lyberadmin/common-accessioning/log/crondebug.log 2>&1
+class EmbargoRelease
 
-require File.expand_path(File.dirname(__FILE__) + '/../../config/boot')
+  # Turn off active_fedora updates of solr
+  ENABLE_SOLR_UPDATES = false
+  LyberCore::Log.set_logfile(File.join(ROBOT_ROOT, 'log', 'embargo_release.log'))
 
-# Turn off active_fedora updates of solr
-ENABLE_SOLR_UPDATES = false
-LyberCore::Log.set_logfile(File.join(ROBOT_ROOT, 'log', 'embargo_release.log'))
+  # Finds druids from solr based on the passed in query
+  # It will then load each item from Dor, and call the block with the item
+  # @param [String] query used to locate druids of items to release from solr
+  # @param [String] embargo_msg embargo type used in log messages (embargo vs 20% visibilty embargo)
+  # @yield [Dor::Item] gets executed after loading the object from DOR and opening new version
+  #  Steps needed to release the particular embargo from the item
+  def self.release_items(query, embargo_msg = 'embargo', &release_block)
+    # Find objects to process
+    LyberCore::Log.info('***** Querying solr: ' << query)
+    solr = Dor::SearchService.query(query, 'rows' => '5000', 'fl' => 'id')
 
-# Finds druids from solr based on the passed in query
-# It will then load each item from Dor, and call the block with the item
-# @param [String] query used to locate druids of items to release from solr
-# @param [String] embargo_msg embargo type used in log messages (embargo vs 20% visibilty embargo)
-# @yield [Dor::Item] gets executed after loading the object from DOR and opening new version
-#  Steps needed to release the particular embargo from the item
-def release_items(query, embargo_msg = 'embargo', &release_block)
-  # Find objects to process
-  LyberCore::Log.info('***** Querying solr: ' << query)
-  solr = Dor::SearchService.query(query, 'rows' => '5000', 'fl' => 'id')
+    num_found = solr['response']['numFound']
+    if (num_found == 0)
+      LyberCore::Log.info('No objects to process')
+      return
+    end
+    LyberCore::Log.info("Found #{num_found} objects")
 
-  num_found = solr['response']['numFound']
-  if (num_found == 0)
-    LyberCore::Log.info('No objects to process')
-    return
+    druid = ''
+    count = 0
+    solr['response']['docs'].each do |doc|
+      begin
+        druid = doc['id']
+        LyberCore::Log.info("Releasing #{embargo_msg} for #{druid}")
+        ei = Dor::Item.find(druid)
+        ei.open_new_version
+        release_block.call(ei)
+        ei.save
+        ei.close_version :description => "#{embargo_msg} released", :significance => :admin
+        count += 1
+      rescue Exception => e
+        msg = "!!! Unable to release embargo for: #{druid}\n" << e.inspect << "\n" << e.backtrace.join("\n")
+        LyberCore::Log.error(msg)
+        Dor::Config.workflow.client.update_workflow_error_status 'dor', druid, 'disseminationWF', 'embargo-release', "#{e.to_s}"
+      end
+    end
+
+    LyberCore::Log.info("Done! Processed #{count} objects out of #{num_found}")
   end
-  LyberCore::Log.info("Found #{num_found} objects")
 
-  druid = ''
-  count = 0
-  solr['response']['docs'].each do |doc|
-    begin
-      druid = doc['id']
-      LyberCore::Log.info("Releasing #{embargo_msg} for #{druid}")
-      ei = Dor::Item.find(druid)
-      ei.open_new_version
-      release_block.call(ei)
-      ei.save
-      ei.close_version :description => "#{embargo_msg} released", :significance => :admin
-      count += 1
-    rescue Exception => e
-      msg = "!!! Unable to release embargo for: #{druid}\n" << e.inspect << "\n" << e.backtrace.join("\n")
-      LyberCore::Log.error(msg)
-      Dor::Config.workflow.client.update_workflow_error_status 'dor', druid, 'disseminationWF', 'embargo-release', "#{e.to_s}"
+  def self.release
+    release_items("embargo_status_ssim:\"embargoed\" AND embargo_release_dtsim:[* TO NOW]") do |item|
+      item.release_embargo('application:accessionWF:embargo-release')
+    end
+
+    release_items("twenty_pct_status_ssim:\"embargoed\" AND twenty_pct_visibility_release_dtsim:[* TO NOW]",
+                  '20% visibility embargo') do |item|
+      item.release_20_pct_vis_embargo('application:accessionWF:embargo-release')
     end
   end
-
-  LyberCore::Log.info("Done! Processed #{count} objects out of #{num_found}")
 end
 
-def release
-  release_items("embargo_status_ssim:\"embargoed\" AND embargo_release_dtsim:[* TO NOW]") do |item|
-    item.release_embargo('application:accessionWF:embargo-release')
-  end
-
-  release_items("twenty_pct_status_ssim:\"embargoed\" AND twenty_pct_visibility_release_dtsim:[* TO NOW]",
-                '20% visibility embargo') do |item|
-    item.release_20_pct_vis_embargo('application:accessionWF:embargo-release')
-  end
-end
-
-release
+EmbargoRelease.release
---
+++ spec/robots/accession/embargo_release_spec.rb
@@ -0,0 +1,132 @@
+require 'spec_helper'
+
+describe 'embargo_release.rb' do
+  Dor.configure do
+    workflow.url 'http://example.org/workflow'
+  end
+
+  before(:each) do
+    allow_any_instance_of(RSolr::Client).to receive(:find) {
+      {
+        'response' => {
+          'numFound' => '1',
+          'docs' => {}
+        }
+      }
+    }
+    # must do require after mocking Solr call because loading the file calls method
+    require File.expand_path(File.dirname(__FILE__) + '/../../../robots/accession/embargo_release')
+  end
+
+  let(:embargo_release_date) { Time.now.utc - 100000 }
+
+  let(:release_access) { <<-EOXML
+    <releaseAccess>
+      <access type="discover">
+        <machine>
+          <world/>
+        </machine>
+      </access>
+      <access type="read">
+        <machine>
+          <world/>
+        </machine>
+      </access>
+    </releaseAccess>
+    EOXML
+  }
+
+  let(:rights_xml) { <<-EOXML
+    <rightsMetadata objectId="druid:rt923jk342">
+      <access type="discover">
+        <machine>
+          <world />
+        </machine>
+      </access>
+      <access type="read">
+        <machine>
+          <group>stanford</group>
+          <embargoReleaseDate>#{embargo_release_date.iso8601}</embargoReleaseDate>
+        </machine>
+      </access>
+    </rightsMetadata>
+    EOXML
+  }
+
+  context "release_embargo" do
+    let(:embargo_xml) { <<-EOXML
+      <embargoMetadata>
+        <status>embargoed</status>
+        <releaseDate>#{embargo_release_date.iso8601}</releaseDate>
+        <twentyPctVisibilityStatus/>
+        <twentyPctVisibilityReleaseDate/>
+        #{release_access}
+      </embargoMetadata>
+      EOXML
+    }
+    let(:item) {
+      i = Dor::Item.new
+      rds = Dor::RightsMetadataDS.new
+      rds.content = Nokogiri::XML(rights_xml) {|config| config.default_xml.noblanks}.to_s
+      i.datastreams['rightsMetadata'] = rds
+      eds = Dor::EmbargoMetadataDS.new
+      eds.content = Nokogiri::XML(embargo_xml) {|config| config.default_xml.noblanks}.to_s
+      i.datastreams['embargoMetadata'] = eds
+      i
+    }
+    it 'rights metadata has no embargo after Dor::Item.release_embargo' do
+      expect(item.rightsMetadata.ng_xml.at_xpath('//embargoReleaseDate')).not_to be_nil
+      expect(item.rightsMetadata.content).to match('embargoReleaseDate')
+      item.release_embargo('ignored')
+      expect(item.rightsMetadata.ng_xml.at_xpath('//embargoReleaseDate')).to be_nil
+      expect(item.rightsMetadata.content).not_to match('embargoReleaseDate')
+    end
+
+    it 'embargo metadata changes to status released after Dor::Item.release_embargo' do
+      expect(item.embargoMetadata.ng_xml.at_xpath('//status').text).to eql 'embargoed'
+      item.release_embargo('ignored')
+      expect(item.embargoMetadata.ng_xml.at_xpath('//status').text).to eql 'released'
+    end
+  end
+
+  context 'release_20_pct_vis_embargo' do
+    let(:embargo_twenty_pct_xml) { <<-EOXML
+      <embargoMetadata>
+        <status>embargoed</status>
+        <releaseDate>#{embargo_release_date.iso8601}</releaseDate>
+        <twentyPctVisibilityStatus>anything</twentyPctVisibilityStatus>
+        <twentyPctVisibilityReleaseDate>#{embargo_release_date.iso8601}</twentyPctVisibilityReleaseDate>
+        #{release_access}
+      </embargoMetadata>
+      EOXML
+    }
+    let(:item) {
+      i = Dor::Item.new
+      rds = Dor::RightsMetadataDS.new
+      rds.content = Nokogiri::XML(rights_xml) {|config| config.default_xml.noblanks}.to_s
+      i.datastreams['rightsMetadata'] = rds
+      eds = Dor::EmbargoMetadataDS.new
+      eds.content = Nokogiri::XML(embargo_twenty_pct_xml) {|config| config.default_xml.noblanks}.to_s
+      i.datastreams['embargoMetadata'] = eds
+      i
+    }
+
+    it 'rights metadata has no embargo after Dor::Item.release_20_pct_vis_embargo' do
+      expect(item.rightsMetadata.ng_xml.at_xpath('//embargoReleaseDate')).not_to be_nil
+      expect(item.rightsMetadata.content).to match('embargoReleaseDate')
+      item.release_20_pct_vis_embargo('ignored')
+      expect(item.rightsMetadata.ng_xml.at_xpath('//embargoReleaseDate')).to be_nil
+      expect(item.rightsMetadata.content).not_to match('embargoReleaseDate')
+    end
+
+    it 'embargo metadata changes to twenty_pct_status released after Dor::Item.release_20_pct_vis_embargo' do
+      expect(item.embargoMetadata.twenty_pct_status).to eql 'anything'
+      item.release_20_pct_vis_embargo('ignored')
+      expect(item.embargoMetadata.twenty_pct_status).to eql 'released'
+    end
+  end
+
+  it 'attempts to run EmbargoRelease.release' do
+    skip('difficult to test, need to include file to set expectation, but including file executes method call to be tested.  right thing to do would be to refactor into class def file and script execution file.')
+  end
+end
