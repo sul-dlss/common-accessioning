bd28437e8f5d1b5704b5a53ac1dee712659c3a23
Tony Zanella <tony.zanella@gmail.com>
2018-06-04 14:47:07 -0700

    Dev machines are gone

--- config/deploy/dev.rb
+++
@@ -1,9 +0,0 @@
-server 'sul-robots1-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots2-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
-set :deploy_environment, 'development'
-set :whenever_environment, fetch(:deploy_environment)
-set :default_env, { :robot_environment => fetch(:deploy_environment) }
-set :whenever_roles, [:db, :app]
