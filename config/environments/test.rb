258f66a55e3c46c5eb1a7b0c18bbee6c4ed24f31
Justin Coyne <jcoyne@justincoyne.com>
2018-12-11 11:16:17 -0800

    Merge item-release robots in

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,48 @@
 # frozen_string_literal: true
 
-REDIS_URL = 'redis_host'
+CERT_DIR = File.join(File.dirname(__FILE__), '../..', 'certs')
+
+Dor::Config.configure do
+  fedora do
+    url       'https://USERNAME:PASSWORD@example.com/fedora'
+  end
+
+  ssl do
+    cert_file File.join(CERT_DIR, 'dlss-dev-test.crt')
+    key_file  File.join(CERT_DIR, 'dlss-dev-test.key')
+    key_pass  ''
+  end
+
+  workflow.url 'https://example.com/workflow/'
+  solr.url 'http://localhost:8983/solr/argo'
+
+  robots do
+    workspace '/tmp'
+  end
+
+  release do
+    fetcher_root 'http://localhost:3000/'
+    workflow_name 'releaseWF'
+    max_tries 5 # the number of attempts to retry service calls before failing
+    max_sleep_seconds   120  # max sleep seconds between tries
+    base_sleep_seconds  10   # base sleep seconds between tries
+  end
+
+  stacks do
+    document_cache_storage_root '/purl/document_cache'
+    document_cache_host 'purl.stanford.edu'
+    local_workspace_root '/dor/workspace'
+    local_stacks_root '/stacks'
+    local_document_cache_root '/purl/document_cache'
+    local_recent_changes '/purl/recent_changes'
+    url 'https://stacks-test.stanford.edu'
+    iiif_profile 'http://iiif.io/api/image/2/level1.json'
+  end
+
+  dor do
+    service_root 'https://USERNAME:PASSWORD@example.com/dor/v1'
+  end
+end
+
+REDIS_URL = '127.0.0.1:6379/resque:development' # hostname:port[:db]/namespace
+# REDIS_TIMEOUT = '5' # seconds
