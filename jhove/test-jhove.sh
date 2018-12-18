9c50e4bd3b8d1f726adaade68c9142bdfaa5fb21
Justin Littman <justinlittman@stanford.edu>
2018-12-18 15:10:16 -0500

    Updated to jhove-service 1.1.4. Removed unused jhove files.

--- jhove/test-jhove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-testdir=/dor/workspace/test/jhove-test
-samples=$testdir/content-files
-output=$testdir/output/jhove-out.xml
-rm -f $output
-JHOVE_HOME=`dirname $0`
-$JHOVE_HOME/jhove.sh $samples > $output
-less $output
