9d809c19ce6aac4fa65764c5f1a204bc4a36583d
aaron-collier <aaron.collier@stanford.edu>
2018-12-19 14:53:33 -0800

    Update deploy to new servers

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-server 'sul-robots1-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db} # only server to get whenever cronjobs deployed to
-server 'sul-robots2-test.stanford.edu', user: 'lyberadmin', roles: %w{web app}
-
+server 'common-accessioning-stage.stanford.edu', user: 'lyberadmin', roles: %w{web app db} # only server to get whenever cronjobs deployed to
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
