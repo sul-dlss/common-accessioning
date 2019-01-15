9710066a2c714b221026a9640b892014b1d957ea
Justin Coyne <jcoyne@justincoyne.com>
2019-01-15 14:18:22 -0600

    Add multi-stage configuration

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-server 'common-accessioning-stage.stanford.edu', user: 'lyberadmin', roles: %w{web app db} # only server to get whenever cronjobs deployed to
+server 'common-accessioning-stage-a.stanford.edu', user: 'lyberadmin', roles: %w{web app db} # only server to get whenever cronjobs deployed to
+server 'common-accessioning-stage-b.stanford.edu', user: 'lyberadmin', roles: %w{web app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
