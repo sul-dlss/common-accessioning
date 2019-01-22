643e1674d54dae32f31dea855f16b80cbe7cc4cc
Justin Coyne <jcoyne@justincoyne.com>
2019-01-22 08:42:59 -0600

    Update the cronjob to use the correct directory

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-set :output, '/home/lyberadmin/common-accessioning/current/log/crondebug.log'
+set :output, '/opt/app/lyberadmin/common-accessioning/current/log/crondebug.log'
 
 every :day, :at => '2:16am', :roles => [:db] do
- command "cd /home/lyberadmin/common-accessioning/current/ && BUNDLE_GEMFILE=/home/lyberadmin/common-accessioning/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/default/ruby /home/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb"
+ command 'cd /opt/app/lyberadmin/common-accessioning/current/ && ' \
+         'BUNDLE_GEMFILE=/opt/app/lyberadmin/common-accessioning/current/Gemfile ' \
+         "ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/default/ruby " \
+         '/opt/app/lyberadmin/common-accessioning/current/robots/accession/embargo_release.rb'
 end
 
 every 5.minutes, :roles => [:app] do
