513e2ec8aaeff55da470ec49defaeb360ff0b382
Justin Coyne <jcoyne@justincoyne.com>
2019-04-09 09:12:40 -0500

    Add production servers

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-server 'common-accessioning-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db} # only server to get whenever cronjobs deployed to
+server 'common-accessioning-prod-a.stanford.edu', user: 'lyberadmin', roles: %w{web app db} # only server to get whenever cronjobs deployed to
+server 'common-accessioning-prod-b.stanford.edu', user: 'lyberadmin', roles: %w{web app}
+server 'common-accessioning-prod-c.stanford.edu', user: 'lyberadmin', roles: %w{web app}
+server 'common-accessioning-prod-d.stanford.edu', user: 'lyberadmin', roles: %w{web app}
+server 'common-accessioning-prod-e.stanford.edu', user: 'lyberadmin', roles: %w{web app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
