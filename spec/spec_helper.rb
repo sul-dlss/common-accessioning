659616738e11e384a441ea34acd1a52bb7accf41
Justin Coyne <jcoyne@justincoyne.com>
2019-04-11 14:49:43 -0500

    Move DatastreamBuilder from dor-services

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ require 'pry'
 require 'rspec'
 require 'webmock/rspec'
 require 'equivalent-xml/rspec_matchers'
+require 'support/foxml_helper'
 
 def setup_release_item(druid, obj_type, members)
   @release_item = Dor::Release::Item.new(druid: druid, skip_heartbeat: true)
@@ -38,4 +39,20 @@ def setup_release_item(druid, obj_type, members)
     members: members
   )
   allow(Dor::Release::Item).to receive_messages(new: @release_item)
+end
+
+def instantiate_fixture(druid, klass = ActiveFedora::Base)
+  mask = File.join(fixture_dir, "*_#{druid.sub(/:/, '_')}.xml")
+  fname = Dir[mask].first
+  return nil if fname.nil?
+
+  item_from_foxml(File.read(fname), klass)
+end
+
+def read_fixture(fname)
+  File.read(File.join(fixture_dir, fname))
+end
+
+def fixture_dir
+  @fixture_dir ||= File.join(File.dirname(__FILE__), 'fixtures')
 end
