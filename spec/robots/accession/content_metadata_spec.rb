e470c732ebc3cd91bb0895e356d3aacb45a6c72a
Justin Coyne <jcoyne@justincoyne.com>
2019-04-16 14:40:55 -0500

    Collections don't get contentMetadata

--- spec/robots/accession/content_metadata_spec.rb
+++ spec/robots/accession/content_metadata_spec.rb
@@ -11,16 +11,28 @@ RSpec.describe Robots::DorRepo::Accession::ContentMetadata do
       allow(Dor).to receive(:find).and_return(object)
     end
     let(:druid) { 'druid:bd185gs2259' }
-    let(:object) { Dor::Item.new(pid: druid) }
     let(:builder) { instance_double(DatastreamBuilder, build: true) }
 
-    it 'builds a datastream' do
-      expect(DatastreamBuilder).to receive(:new)
-        .with(datastream: Dor::ContentMetadataDS,
-              force: true,
-              object: object).and_return(builder)
-      expect(builder).to receive(:build)
-      perform
+    context 'on an item' do
+      let(:object) { Dor::Item.new(pid: druid) }
+
+      it 'builds a datastream' do
+        expect(DatastreamBuilder).to receive(:new)
+          .with(datastream: Dor::ContentMetadataDS,
+                force: true,
+                object: object).and_return(builder)
+        expect(builder).to receive(:build)
+        perform
+      end
+    end
+
+    context 'on a collection' do
+      let(:object) { Dor::Collection.new(pid: druid) }
+
+      it "doesn't make a datastream" do
+        expect(DatastreamBuilder).not_to receive(:new)
+        perform
+      end
     end
   end
 end
