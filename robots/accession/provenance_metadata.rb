d451e50636660abdd8de020a37a0bcc2d97cb040
Justin Coyne <jcoyne@justincoyne.com>
2019-04-17 11:01:27 -0500

    Move provenance datastream building from dor-services

--- robots/accession/provenance_metadata.rb
+++ robots/accession/provenance_metadata.rb
@@ -11,7 +11,33 @@ module Robots
 
         def perform(druid)
           obj = Dor.find(druid)
-          obj.build_provenanceMetadata_datastream('accessionWF', 'DOR Common Accessioning completed')
+          build_datastream(obj, 'accessionWF', 'DOR Common Accessioning completed')
+        end
+
+        private
+
+        def build_datastream(obj, workflow_id, event_text)
+          workflow_provenance = create_workflow_provenance(obj.pid, workflow_id, event_text)
+          ds = obj.provenanceMetadata
+          ds.label ||= 'Provenance Metadata'
+          ds.ng_xml = workflow_provenance
+          ds.save
+        end
+
+        # @return [Nokogiri::Document]
+        def create_workflow_provenance(druid, workflow_id, event_text)
+          builder = Nokogiri::XML::Builder.new do |xml|
+            xml.provenanceMetadata(objectId: druid) do
+              xml.agent(name: 'DOR') do
+                xml.what(object: druid) do
+                  xml.event(who: "DOR-#{workflow_id}", when: Time.new.iso8601) do
+                    xml.text(event_text)
+                  end
+                end
+              end
+            end
+          end
+          builder.doc
         end
       end
     end
--- spec/robots/accession/provenance_metadata_spec.rb
+++ spec/robots/accession/provenance_metadata_spec.rb
@@ -2,16 +2,60 @@
 
 require 'spec_helper'
 
-require File.expand_path(File.dirname(__FILE__) + '/../../../robots/accession/provenance_metadata')
+RSpec.describe Robots::DorRepo::Accession::ProvenanceMetadata do
+  let(:robot) { Robots::DorRepo::Accession::ProvenanceMetadata.new }
 
-describe Robots::DorRepo::Accession::ProvenanceMetadata do
   it 'includes behavior from LyberCore::Robot' do
-    robot = Robots::DorRepo::Accession::ProvenanceMetadata.new
     expect(robot.methods).to include(:work)
   end
 
   it 'has a ROBOT_ROOT' do
     guessed_robot_root = File.expand_path(File.dirname(__FILE__) + '/../../..')
     expect(ROBOT_ROOT).to eql(guessed_robot_root)
+  end
+
+  describe 'build_datastream' do
+    subject(:build) { robot.send(:build_datastream, item, workflow_id, event_text) }
+
+    let(:workflow_id) { 'accessionWF' }
+    let(:event_text) { 'DOR Common Accessioning' }
+
+    context 'on an existing object' do
+      let(:item) { instantiate_fixture('druid:ab123cd4567', Dor::Item) }
+
+      it 'builds the provenanceMetadata datastream' do
+        expect(item.datastreams['provenanceMetadata'].ng_xml.to_s).to be_equivalent_to('<xml/>')
+        build
+        expect(item.datastreams['provenanceMetadata'].ng_xml.to_s).not_to be_equivalent_to('<xml/>')
+      end
+    end
+
+    context 'on a new object' do
+      let(:item) { Dor::Item.new(pid: druid) }
+      let(:druid) { 'druid:aa123bb4567' }
+
+      before do
+        # stub fedora
+        allow(item.inner_object).to receive(:repository).and_return(double('frepo').as_null_object)
+      end
+
+      it 'generates workflow provenance' do
+        build
+        wp_xml = item.datastreams['provenanceMetadata'].ng_xml
+        expect(wp_xml).to be_instance_of(Nokogiri::XML::Document)
+        expect(wp_xml.root.name).to eql('provenanceMetadata')
+        expect(wp_xml.root[:objectId]).to eql(druid)
+        agent = wp_xml.xpath('/provenanceMetadata/agent').first
+        expect(agent.name).to eql('agent')
+        expect(agent[:name]).to eql('DOR')
+        what = agent.first_element_child
+        expect(what.name).to eql('what')
+        expect(what[:object]).to eql(druid)
+        event = what.first_element_child
+        expect(event.name).to eql('event')
+        expect(event[:who]).to eql("DOR-#{workflow_id}")
+        expect(event.content).to eql(event_text)
+      end
+    end
   end
 end
