d599ccfdf37862c603ffd05bfe54050bc5296895
Justin Coyne <jcoyne@justincoyne.com>
2019-05-06 09:45:55 -0500

    Add a test for Dissemination::Cleanup

--- robots/dissemination/cleanup.rb
+++ robots/dissemination/cleanup.rb
@@ -4,13 +4,12 @@ module Robots
   module DorRepo
     module Dissemination
       class Cleanup < Robots::DorRepo::Base
-
         def initialize
           super('dor', 'disseminationWF', 'cleanup')
         end
 
         def perform(druid)
-          #decide whether the druid is full or truncated
+          # decide whether the druid is full or truncated
           if is_full_druid_tree(druid)
             CleanupResetService.cleanup_by_reset_druid druid
           else
@@ -18,7 +17,7 @@ module Robots
           end
         end
 
-        #determines if the druid is the regular druid tree or the truncated one
+        # determines if the druid is the regular druid tree or the truncated one
         def is_full_druid_tree(druid)
           workspace_root = Dor::Config.cleanup.local_workspace_root
           full_druid_tree = DruidTools::Druid.new(druid, workspace_root)
---
+++ spec/robots/dissemination/cleanup_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Robots::DorRepo::Dissemination::Cleanup do
+  subject(:robot) { described_class.new }
+  let(:druid) { 'druid:aa222cc3333' }
+
+  describe '#perform' do
+    before do
+      allow(Dor::CleanupService).to receive(:cleanup_by_druid)
+    end
+
+    it 'is successful' do
+      robot.perform(druid)
+      expect(Dor::CleanupService).to have_received(:cleanup_by_druid).with(druid)
+    end
+  end
+end
