96f040a1adf7758b51c0ae85745a47ed06fdfe05
Tony Zanella <tony.zanella@gmail.com>
2019-05-15 11:06:28 -0700

    Remove token setup for afs
      since pickup location is in nfs

--- bin/run_marc_builder.sh
+++ bin/run_marc_builder.sh
@@ -7,14 +7,4 @@
 # for a temporary lock file
 #
 
-
-#
-# Make sure the kerberos ticket and afs token are renewed
-#
-echo "renew-ticket for afs"
-date
-KRB5CCNAME=/tmp/kerb5.tkt KINIT_PROG=/usr/bin/aklog \
-/usr/local/bin/k5start  -t -k /tmp/kerb5.tkt -H 60 -U -f \
-/opt/app/lyberadmin/sulair-lyberservices
-
 /opt/app/lyberadmin/common-accessioning/current/robots/etd_submit/build_symphony_marc.rb
