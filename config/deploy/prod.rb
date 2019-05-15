1b564f81aa0f112e524f69c6c97e5b099c8dcbc9
Justin Littman <justinlittman@stanford.edu>
2019-05-15 09:22:55 -0400

    Switched workers to app so that shared_config works.

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-server 'common-accessioning-prod-a.stanford.edu', user: 'lyberadmin', roles: %w{workers cron} # only server to get whenever cronjobs deployed to
-server 'common-accessioning-prod-b.stanford.edu', user: 'lyberadmin', roles: %w{workers}
-server 'common-accessioning-prod-c.stanford.edu', user: 'lyberadmin', roles: %w{workers}
-server 'common-accessioning-prod-d.stanford.edu', user: 'lyberadmin', roles: %w{workers}
-server 'common-accessioning-prod-e.stanford.edu', user: 'lyberadmin', roles: %w{workers}
+server 'common-accessioning-prod-a.stanford.edu', user: 'lyberadmin', roles: %w{app cron} # only server to get whenever cronjobs deployed to
+server 'common-accessioning-prod-b.stanford.edu', user: 'lyberadmin', roles: %w{app}
+server 'common-accessioning-prod-c.stanford.edu', user: 'lyberadmin', roles: %w{app}
+server 'common-accessioning-prod-d.stanford.edu', user: 'lyberadmin', roles: %w{app}
+server 'common-accessioning-prod-e.stanford.edu', user: 'lyberadmin', roles: %w{app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
