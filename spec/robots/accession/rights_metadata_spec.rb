f1cfe9d6cae5042af26185e5f90f588ad0904a15
Justin Coyne <jcoyne@justincoyne.com>
2020-01-28 17:37:01 -0600

    Remove a Dor.find from rightsMetadata in favor of the cocina api
    This decouples common-accessioning from Fedora

--- spec/robots/accession/rights_metadata_spec.rb
+++ spec/robots/accession/rights_metadata_spec.rb
@@ -7,26 +7,41 @@ RSpec.describe Robots::DorRepo::Accession::RightsMetadata do
 
   let(:robot) { described_class.new }
   let(:druid) { 'druid:ab123cd4567' }
+  let(:apo_id) { 'druid:mx121xx1234' }
+  let(:object) do
+    Cocina::Models::DRO.new(externalIdentifier: '123',
+                            type: Cocina::Models::DRO::TYPES.first,
+                            label: 'my repository object',
+                            version: 1,
+                            administrative: { hasAdminPolicy: apo_id })
+  end
 
   let(:object_client) do
-    instance_double(Dor::Services::Client::Object, refresh_metadata: true, metadata: metadata_client)
+    instance_double(Dor::Services::Client::Object, refresh_metadata: true, metadata: metadata_client, find: object)
   end
   let(:metadata_client) do
     instance_double(Dor::Services::Client::Metadata, legacy_update: true)
   end
 
   before do
-    allow(Dor::Services::Client).to receive(:object).and_return(object_client)
+    allow(Dor::Services::Client).to receive(:object).with(druid).and_return(object_client)
   end
 
   context 'when no rightsMetadata file is found' do
+    let(:apo_object_client) { instance_double(Dor::Services::Client::Object, find: apo) }
+    let(:apo) do
+      Cocina::Models::AdminPolicy.new(externalIdentifier: '123',
+                                      type: Cocina::Models::AdminPolicy::TYPES.first,
+                                      label: 'my apo object',
+                                      version: 1,
+                                      administrative: {})
+    end
+    let(:fedora_obj) { instance_double(Dor::Item, rightsMetadata: datastream) }
+
     before do
       allow(Dor).to receive(:find).and_return(fedora_obj)
+      allow(Dor::Services::Client).to receive(:object).with(apo_id).and_return(apo_object_client)
     end
-
-    let(:fedora_obj) { instance_double(Dor::Item, rightsMetadata: datastream, admin_policy_object: apo) }
-    let(:apo) { instance_double(Dor::AdminPolicyObject, defaultObjectRights: default_ds) }
-    let(:default_ds) { instance_double(Dor::DefaultObjectRightsDS, content: 'this is default') }
 
     context "when rightsMetadata doesn't exist" do
       let(:datastream) { instance_double(Dor::RightsMetadataDS, new?: true) }
