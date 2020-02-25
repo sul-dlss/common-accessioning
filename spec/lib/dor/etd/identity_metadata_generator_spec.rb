6c94ab1a75afa1cda5b829deaa1fa908b26578b8
Naomi Dushay <ndushay@stanford.edu>
2020-02-25 14:52:39 -0800

    add spec for identity_metadata_generator and fix a bug

---
+++ spec/lib/dor/etd/identity_metadata_generator_spec.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Dor::Etd::IdentityMetadataGenerator do
+  subject(:generate) { described_class.generate(etd) }
+
+  let(:etd) { Etd.new(pid: druid) }
+  let(:druid) { 'druid:ab123cd4567' }
+  let(:new_uuid) { UUIDTools::UUID.timestamp_create }
+  let(:old_id_metadata) { instance_double('identityMetadata') }
+  # Can't make this a verified double because it's a ActiveFedora::SimpleDatastream (which does a bunch of method missing stuff)
+  # rubocop:disable RSpec/VerifiedDoubles
+  let(:properties_ds) { double('custom properties', dissertation_id: ['0000005666'], etd_type: ['Dissertation']) }
+  # rubocop:enable RSpec/VerifiedDoubles
+
+  before do
+    allow(etd).to receive(:datastreams).and_return('properties' => properties_ds)
+    allow(etd).to receive(:identityMetadata).and_return(old_id_metadata)
+    allow(UUIDTools::UUID).to receive(:timestamp_create).and_return(new_uuid)
+  end
+
+  describe '.generate' do
+    context 'when creating identityMetadata' do
+      before do
+        allow(old_id_metadata).to receive(:new?).and_return(true)
+      end
+
+      it 'generates xml' do
+        expect(generate).to be_equivalent_to <<~XML
+          <?xml version="1.0"?>
+          <identityMetadata>
+            <objectId>druid:ab123cd4567</objectId>
+            <objectType>item</objectType>
+            <objectLabel></objectLabel>
+            <objectCreator>DOR</objectCreator>
+            <otherId name="dissertationid">0000005666</otherId>
+            <otherId name="catkey"/>
+            <otherId name="uuid">#{new_uuid}</otherId>
+            <agreementId>druid:ct692vv3660</agreementId>
+            <objectAdminClass>ETDs</objectAdminClass>
+            <tag>ETD : Dissertation</tag>
+          </identityMetadata>
+        XML
+      end
+    end
+
+    context 'when updating identityMetadata' do
+      before do
+        allow(old_id_metadata).to receive(:new?).and_return(false)
+        allow(old_id_metadata).to receive(:content).and_return(prev_xml)
+      end
+
+      context 'when catkey element in previous xml' do
+        let(:prev_xml) do
+          <<~XML
+            <identityMetadata>
+              <catkey>666</catkey>
+            </identityMetadata>
+          XML
+        end
+
+        it 'includes catkey value' do
+          expect(generate).to include('<otherId name="catkey">666</otherId>')
+        end
+      end
+
+      context 'when catkey via otherId[@name="catkey"]' do
+        let(:prev_xml) do
+          <<~XML
+            <identityMetadata>
+              <otherId name='catkey'>999</otherId>
+            </identityMetadata>
+          XML
+        end
+
+        it 'includes catkey value' do
+          expect(generate).to include('<otherId name="catkey">999</otherId>')
+        end
+      end
+
+      context 'when previous uuid via otherId[@name="uuid"]' do
+        let(:prev_xml) do
+          <<~XML
+            <identityMetadata>
+              <otherId name='uuid'>i-are-a-uuid</otherId>
+              <catkey>666</catkey>
+            </identityMetadata>
+          XML
+        end
+
+        it 'includes previous uuid value' do
+          expect(generate).to include('<otherId name="uuid">i-are-a-uuid</otherId>')
+        end
+      end
+
+      context 'when previous uuid was blank' do
+        let(:prev_xml) do
+          <<~XML
+            <identityMetadata>
+              <otherId name='uuid'></otherId>
+              <catkey>666</catkey>
+            </identityMetadata>
+          XML
+        end
+
+        it 'creates new uuid value' do
+          expect(generate).to include("<otherId name=\"uuid\">#{new_uuid}</otherId>")
+        end
+      end
+
+      context 'when no previous uuid' do
+        let(:prev_xml) do
+          <<~XML
+            <identityMetadata>
+              <catkey>666</catkey>
+            </identityMetadata>
+          XML
+        end
+
+        it 'creates new uuid value' do
+          expect(generate).to include("<otherId name=\"uuid\">#{new_uuid}</otherId>")
+        end
+      end
+    end
+  end
+end
