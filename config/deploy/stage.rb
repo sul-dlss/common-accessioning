2d858df6bbd4e2c28e54a41ecafe19918110e5ee
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-04-13 10:52:05 -0700

    Fix honeybadger, take three
    Apparently the setting in deploy.rb does not take effect in specific envs.

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -7,3 +7,6 @@ Capistrano::OneTimeKey.generate_one_time_key!
 
 set :deploy_environment, 'production'
 set :default_env, robot_environment: fetch(:deploy_environment)
+# See https://github.com/honeybadger-io/honeybadger-ruby/issues/129 &
+# https://github.com/honeybadger-io/honeybadger-ruby/blob/7eea24a47d44aed663e315be970e501b7cf092fc/vendor/capistrano-honeybadger/README.md
+set :honeybadger_server, primary(:app)
