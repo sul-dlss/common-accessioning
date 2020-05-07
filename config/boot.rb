b1053526c9c78fb56a10015394bf80e9c6f2f058
Justin Littman <justinlittman@stanford.edu>
2020-05-07 07:36:00 -0400

    Enables dor-services-client get retries.

--- config/boot.rb
+++ config/boot.rb
@@ -44,7 +44,8 @@ require env_file
 module CommonAccessioning
   def self.connect_dor_services_app
     Dor::Services::Client.configure(url: Settings.dor_services.url,
-                                    token: Settings.dor_services.token)
+                                    token: Settings.dor_services.token,
+                                    enable_get_retries: true)
   end
 end
 
