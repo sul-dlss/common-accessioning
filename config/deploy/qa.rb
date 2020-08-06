a5fe790f8a42db5f9eca61118e79cc417cdd7da9
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-08-06 13:51:45 -0700

    Add QA deployment target

---
+++ config/deploy/qa.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+server 'common-accessioning-qa-a.stanford.edu', user: 'lyberadmin', roles: %w[app]
+server 'common-accessioning-qa-b.stanford.edu', user: 'lyberadmin', roles: %w[app]
+
+Capistrano::OneTimeKey.generate_one_time_key!
+
+set :deploy_environment, 'production'
+set :default_env, robot_environment: fetch(:deploy_environment)
+# See https://github.com/honeybadger-io/honeybadger-ruby/issues/129 &
+# https://github.com/honeybadger-io/honeybadger-ruby/blob/7eea24a47d44aed663e315be970e501b7cf092fc/vendor/capistrano-honeybadger/README.md
+set :honeybadger_server, primary(:app)
