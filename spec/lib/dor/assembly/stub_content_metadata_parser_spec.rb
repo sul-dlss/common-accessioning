341050b14040134dfef45f644e587db3d1e51791
Peter Mangiafico <peter@mangiafico.org>
2021-06-23 16:03:36 -0700

    pass reading order to gem when generating content metadata from stub

--- spec/lib/dor/assembly/stub_content_metadata_parser_spec.rb
+++ spec/lib/dor/assembly/stub_content_metadata_parser_spec.rb
@@ -5,7 +5,9 @@ require 'spec_helper'
 RSpec.describe Dor::Assembly::StubContentMetadataParser do
   describe '#stub_content_metadata_parser' do
     let(:druid) { 'druid:aa111bb3333' }
+    let(:druid_rtl) { 'druid:aa111bb5555' }
     let(:item) { Dor::Assembly::Item.new(druid: druid) }
+    let(:item_rtl) { Dor::Assembly::Item.new(druid: druid_rtl) }
 
     let(:xml_doc) do
       <<-EOXML
@@ -30,11 +32,45 @@ RSpec.describe Dor::Assembly::StubContentMetadataParser do
       EOXML
     end
 
-    context 'when stub_object_type is book' do
+    let(:xml_doc_rtl) do
+      <<-EOXML
+      <?xml version="1.0"?>
+      <contentMetadata objectId="druid:aa111bb5555" type="book">
+        <bookData readingOrder="rtl"/>
+        <resource id="aa111bb5555_1" sequence="1" type="object">
+          <label>Optional label</label>
+          <file id="page1.tif" preserve="yes" shelve="no" publish="no"/>
+          <file id="page1.txt" preserve="no" shelve="no" publish="no"/>
+        </resource>
+        <resource id="aa111bb5555_2" sequence="2" type="object">
+          <label>optional page 2 label</label>
+          <file id="page2.tif" preserve="yes" shelve="no" publish="no"/>
+          <file id="some_filename.txt" preserve="yes" shelve="no" publish="no" role="transcription"/>
+        </resource>
+        <resource id="aa111bb5555_3" sequence="3" type="object">
+          <label>Object 3</label>
+          <file id="whole_book.pdf" preserve="no" shelve="no" publish="no"/>
+        </resource>
+      </contentMetadata>
+      EOXML
+    end
+
+    context 'when stub_object_type is book (ltr)' do
       it 'maps content metadata types to book correctly' do
-        ['flipbook (r-l)', 'book', 'a book (l-r)'].each do |content_type|
+        ['book', 'a book (l-r)', 'book (ltr)'].each do |content_type|
           allow(item).to receive(:stub_object_type).and_return(content_type)
           expect(item.gem_content_metadata_style).to eq(:simple_book)
+          expect(item.book_reading_order).to eq('ltr')
+        end
+      end
+    end
+
+    context 'when stub_object_type is book (rtl)' do
+      it 'maps content metadata types to book correctly' do
+        ['flipbook (r-l)', 'book (rtl)'].each do |content_type|
+          allow(item).to receive(:stub_object_type).and_return(content_type)
+          expect(item.gem_content_metadata_style).to eq(:simple_book)
+          expect(item.book_reading_order).to eq('rtl')
         end
       end
     end
@@ -71,10 +107,17 @@ RSpec.describe Dor::Assembly::StubContentMetadataParser do
       end
     end
 
-    context 'when convert_stub_content_metadata method is called' do
+    context 'when convert_stub_content_metadata method is called for an ltr book' do
       it 'generates stub content metadata' do
         stub_xml = item.convert_stub_content_metadata
         expect(stub_xml).to be_equivalent_to xml_doc
+      end
+    end
+
+    context 'when convert_stub_content_metadata method is called for an rtl book' do
+      it 'generates stub content metadata' do
+        stub_xml = item_rtl.convert_stub_content_metadata
+        expect(stub_xml).to be_equivalent_to xml_doc_rtl
       end
     end
   end
