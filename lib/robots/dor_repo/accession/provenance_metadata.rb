eaed3fb5ad4cbb039080a0eb34f6d3d2de852d9d
Justin Coyne <jcoyne@justincoyne.com>
2021-11-03 16:43:14 -0500

    Make provenance-metadata a no op
    In a meeting today, Andrew agreed this step is not useful as this data is duplicative of what is recorded in the events service

--- lib/robots/dor_repo/accession/provenance_metadata.rb
+++ lib/robots/dor_repo/accession/provenance_metadata.rb
@@ -8,33 +8,8 @@ module Robots
           super('accessionWF', 'provenance-metadata')
         end
 
-        def perform(druid)
-          workflow_provenance = create_workflow_provenance(druid)
-          object_client = Dor::Services::Client.object(druid)
-          object_client.metadata.legacy_update(
-            provenance: {
-              updated: Time.now,
-              content: workflow_provenance
-            }
-          )
-        end
-
-        private
-
-        # @return [String]
-        def create_workflow_provenance(druid, time: Time.new.iso8601)
-          builder = Nokogiri::XML::Builder.new do |xml|
-            xml.provenanceMetadata(objectId: druid) do
-              xml.agent(name: 'DOR') do
-                xml.what(object: druid) do
-                  xml.event(who: 'DOR-accessionWF', when: time) do
-                    xml.text('DOR Common Accessioning completed')
-                  end
-                end
-              end
-            end
-          end
-          builder.doc.to_s
+        def perform(_druid)
+          LyberCore::Robot::ReturnState.new(status: :skipped, note: 'This robot no longer does anything')
         end
       end
     end
