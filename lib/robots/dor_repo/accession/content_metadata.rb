ba3796c86b085a02daf3ab15bcd238d9e833558d
Naomi Dushay <ndushay@stanford.edu>
2024-02-23 16:56:16 -0800

    Assembly workflow:  remove noise comments
    "When you read the next line of code out loud, does it tell you exactly what was in the comment?"

--- lib/robots/dor_repo/accession/content_metadata.rb
+++ lib/robots/dor_repo/accession/content_metadata.rb
@@ -4,15 +4,14 @@ module Robots
   module DorRepo
     module Accession
       # Creates structural metadata based on contentMetadata.xml
-      # Needed by https://github.com/sul-dlss/gis-robot-suite/blob/main/lib/robots/dor_repo/gis_assembly/generate_content_metadata.rb
-      # and https://github.com/sul-dlss/was_robot_suite/blob/2575d1d43e3c99a7e6cef28886b433f5a41e14ad/lib/robots/dor_repo/was_seed_preassembly/content_metadata_generator.rb#L12-L13
+      # Needed by https://github.com/sul-dlss/was_robot_suite/blob/main/lib/robots/dor_repo/was_seed_preassembly/content_metadata_generator.rb#L12-L13
       class ContentMetadata < LyberCore::Robot
         def initialize
           super('accessionWF', 'content-metadata')
         end
 
         def perform_work
-          # Objects that aren't items/DROs do not have content metadata attached
+          # Objects that aren't items/DROs do not have files
           return unless cocina_object.dro?
 
           path = druid_object.find_metadata('contentMetadata.xml')
