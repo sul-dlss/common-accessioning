8babdc882ee808546f9eeb4815f4a542f35c12ed
Peter Mangiafico <peter@mangiafico.org>
2024-05-08 14:38:11 -0700

    move abbyy spec to its own file; put ocr output in its own subfolder by druid

--- lib/dor/text_extraction/abbyy.rb
+++ lib/dor/text_extraction/abbyy.rb
@@ -4,13 +4,15 @@ module Dor
   module TextExtraction
     # Build Abbyy tickets for processing
     class Abbyy
+      attr_reader :filepaths, :druid
+
       def initialize(filepaths:, druid:)
         @filepaths = filepaths
         @druid = druid
       end
 
       def abbyy_ticket_filepath
-        File.join(Settings.sdr.abbyy_ticket_path, "#{@druid}.xml")
+        File.join(Settings.sdr.abbyy_ticket_path, "#{druid}.xml")
       end
 
       def image_abbyy
@@ -18,12 +20,16 @@ module Dor
       end
 
       def pdf_files?
-        File.extname(@filepaths.first).strip.downcase[1..] == 'pdf'
+        File.extname(filepaths.first).strip.downcase[1..] == 'pdf'
+      end
+
+      def output_file_path
+        File.join(Settings.sdr.abbyy_output_path, druid).encode(xml: :text)
       end
 
       def alto_output
         "<ExportFormat OutputFileFormat='ALTO' OutputFlowType='SharedFolder' FormatVersion='3_1' CoordinatesParticularity='Words' WriteWordConfidence='true'>
-          <OutputLocation>#{Settings.sdr.abbyy_output_path.encode(xml: :text)}</OutputLocation>
+          <OutputLocation>#{output_file_path}</OutputLocation>
           <FileExistsAction>Overwrite</FileExistsAction>
           <NamingRule>&lt;FileName&gt;.&lt;Ext&gt;</NamingRule>
         </ExportFormat>"
@@ -31,7 +37,7 @@ module Dor
 
       def text_output
         "<ExportFormat OutputFileFormat='Text' OutputFlowType='SharedFolder' EncodingType='UTF8'>
-          <OutputLocation>#{Settings.sdr.abbyy_output_path.encode(xml: :text)}</OutputLocation>
+          <OutputLocation>#{output_file_path}</OutputLocation>
           <FileExistsAction>Overwrite</FileExistsAction>
           <NamingRule>&lt;FileName&gt;.&lt;Ext&gt;</NamingRule>
         </ExportFormat>"
@@ -42,14 +48,14 @@ module Dor
                        PdfAComplianceMode='PDFAM_PdfA_2u' PdfVersion='Version17'
                        Scenario='MaxQuality' UseImprovedCompression='true' MRCMode='Normal'
                        PdfUACompatible='true'>
-          <OutputLocation>#{Settings.sdr.abbyy_output_path.encode(xml: :text)}</OutputLocation>
+          <OutputLocation>#{output_file_path}</OutputLocation>
           <FileExistsAction>Overwrite</FileExistsAction>
           <NamingRule>&lt;FileName&gt;.&lt;Ext&gt;</NamingRule>
         </ExportFormat>"
       end
 
       def input_filepaths_field
-        @filepaths.map { |filename| "<InputFile Name=#{filename.encode(xml: :attr)}/>" }.join("\n")
+        filepaths.map { |filename| "<InputFile Name=#{filename.encode(xml: :attr)}/>" }.join("\n")
       end
 
       def xml_ticket
--- spec/robots/dor_repo/ocr/ocr_create_spec.rb
+++ spec/robots/dor_repo/ocr/ocr_create_spec.rb
@@ -5,38 +5,8 @@ require 'spec_helper'
 describe Robots::DorRepo::Ocr::OcrCreate do
   let(:druid) { 'druid:bb222cc3333' }
   let(:robot) { described_class.new }
-  let(:abbyy) { Dor::TextExtraction::Abbyy.new(filepaths:, druid:) }
-  let(:xml_contents) { parse_xml(abbyy.abbyy_ticket_filepath) }
-  let(:fixture_path) { File.join(File.absolute_path('spec/fixtures/ocr'), "#{druid}_abbyy_ticket.xml") }
-
-  def parse_xml(filepath)
-    Nokogiri::XML(File.read(filepath)).to_xml.gsub(/\s+/, ' ').strip
-  end
 
   it 'runs the OCR create robot' do
     expect(test_perform(robot, druid)).to be true
-  end
-
-  context 'with abbyy xml ticket' do
-    before do
-      abbyy.xml_ticket
-    end
-
-    context 'when the files are images' do
-      let(:filepaths) { %w[/with/path/filename1.jp2 filename2.jp2 filename3.jp2] }
-
-      it 'creates xml for files for images' do
-        expect(xml_contents).to eq parse_xml(fixture_path)
-      end
-    end
-
-    context 'when the files are not images' do
-      let(:filepaths) { %w[filename3.PDF filename4.pdf] }
-      let(:druid) { 'druid:new_druid' }
-
-      it 'creates xml for files for pdfs' do
-        expect(xml_contents).to eq parse_xml(fixture_path)
-      end
-    end
   end
 end
