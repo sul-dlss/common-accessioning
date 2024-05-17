7b95d2ff207de7b0736562e1aae500556fd38b14
Peter Mangiafico <peter@mangiafico.org>
2024-05-17 14:33:59 -0700

    write XML ABBYY ticket in robot step

--- lib/robots/dor_repo/ocr/xml_ticket_create.rb
+++ lib/robots/dor_repo/ocr/xml_ticket_create.rb
@@ -11,7 +11,8 @@ module Robots
 
         # available from LyberCore::Robot: druid, bare_druid, workflow_service, object_client, cocina_object, logger
         def perform_work
-          # generate and write the XML ticket to the file system
+          filepaths = Dor::TextExtraction::Ocr.new(cocina_object:, workflow_context: workflow.context).filenames_to_ocr
+          Dor::TextExtraction::Abbyy::Ticket.new(filepaths:, druid:).write_xml
         end
       end
     end
