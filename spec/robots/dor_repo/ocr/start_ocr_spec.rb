0913d6df6c1b6b73c95b737afcc1ea445e058257
Peter Mangiafico <peter@mangiafico.org>
2024-06-19 15:48:28 -0700

    adds a test for retry logic on open

--- spec/robots/dor_repo/ocr/start_ocr_spec.rb
+++ spec/robots/dor_repo/ocr/start_ocr_spec.rb
@@ -11,8 +11,8 @@ describe Robots::DorRepo::Ocr::StartOcr do
   let(:object) { build(:dro, id: druid) }
   let(:workspace_client) { instance_double(Dor::Services::Client::Workspace) }
   let(:version_client) do
-    instance_double(Dor::Services::Client::ObjectVersion, open: true,
-                                                          status: instance_double(Dor::Services::Client::ObjectVersion::VersionStatus, open?: version_open))
+    instance_double(Dor::Services::Client::ObjectVersion,
+                    status: instance_double(Dor::Services::Client::ObjectVersion::VersionStatus, open?: version_open))
   end
   let(:object_client) do
     instance_double(Dor::Services::Client::Object, version: version_client, workspace: workspace_client, find: object)
@@ -28,9 +28,46 @@ describe Robots::DorRepo::Ocr::StartOcr do
     let(:version_open) { false }
     let(:possible) { true }
 
+    before { allow(version_client).to receive(:open).and_return(true) }
+
     it 'opens the object' do
       perform
       expect(version_client).to have_received(:open)
+    end
+  end
+
+  context 'when the object is not opened and it is possible to OCR, but open version fails' do
+    let(:version_open) { false }
+    let(:possible) { true }
+
+    before { allow(Honeybadger).to receive(:notify) }
+
+    context 'when open version fails twice and then succeeds' do
+      before do
+        count = 0
+        allow(version_client).to receive(:open) do |*_args|
+          count += 1
+          raise Dor::Services::Client::Error unless count > 2
+
+          true
+        end
+      end
+
+      it 'retries the first two errors, then calls open again and logs to honeybadger' do
+        expect { perform }.not_to raise_error
+        expect(version_client).to have_received(:open).thrice # shakespeare coding (first two times fail, third time succeeds)
+        expect(Honeybadger).to have_received(:notify).twice # two calls to HB for the first two failures
+      end
+    end
+
+    context 'when open version fails and exceeds maximum tries' do
+      before { allow(version_client).to receive(:open).and_raise(Dor::Services::Client::Error) }
+
+      it 'logs to honeybadger and then raises the error' do
+        expect { perform }.to raise_error(Dor::Services::Client::Error)
+        expect(version_client).to have_received(:open).thrice # shakespeare coding (all three times fail)
+        expect(Honeybadger).to have_received(:notify).twice # two calls to HB for the first two failures
+      end
     end
   end
 
@@ -62,6 +99,8 @@ describe Robots::DorRepo::Ocr::StartOcr do
   context 'when the object is already opened' do
     let(:version_open) { true }
     let(:possible) { true }
+
+    before { allow(version_client).to receive(:open) }
 
     it 'does nothing' do
       perform
