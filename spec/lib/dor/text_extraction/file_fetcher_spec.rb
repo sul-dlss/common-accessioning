141598320498d90dcf3d7849b53012f6d67789eb
Peter Mangiafico <peter@mangiafico.org>
2024-09-19 17:08:39 -0700

    use local tmp directory

--- spec/lib/dor/text_extraction/file_fetcher_spec.rb
+++ spec/lib/dor/text_extraction/file_fetcher_spec.rb
@@ -8,12 +8,18 @@ describe Dor::TextExtraction::FileFetcher do
   let(:logger) { instance_double(Logger, warn: nil, info: nil, error: nil) }
   let(:druid) { 'druid:bb222cc3333' }
   let(:objects_client) { instance_double(Preservation::Client::Objects) }
+  let(:filename) { 'image111.tif' }
+  let(:base_dir) { 'tmp/b22cc3333' }
+  let(:file_path) { File.join(base_dir, filename) }
+  let(:path) { Pathname.new(file_path) }
+  let(:cloud_endpoint) { 's3://some-bucket/file1.mov' }
 
   let(:pres_client) do
     instance_double(Preservation::Client, objects: objects_client)
   end
 
   before do
+    FileUtils.mkdir_p(base_dir) unless File.directory?(base_dir)
     allow(Preservation::Client).to receive(:configure).and_return(pres_client)
     allow(file_fetcher).to receive(:sleep) # effectively make the sleep a no-op so that the test doesn't take so long due to retries and backoff
   end
@@ -29,13 +35,12 @@ describe Dor::TextExtraction::FileFetcher do
         end
 
         it 'writes the file' do
-          file_fetcher.write_file_with_retries(filename: 'image111.tif', path: Pathname.new('/tmp/image111.tif'))
+          file_fetcher.write_file_with_retries(filename:, path:)
           expect(Preservation::Client).to have_received(:configure)
           expect(logger).to have_received(:info).once
           expect(objects_client).to have_received(:content).once # success first time!
 
-          file1 = File.join(Settings.sdr.abbyy.local_ticket_path, 'bb222cc3333', 'image111.tif')
-          expect(File.read(file1)).to eq('Content for: image111.tif')
+          expect(File.read(file_path)).to eq("Content for: #{filename}")
         end
       end
 
@@ -52,14 +57,13 @@ describe Dor::TextExtraction::FileFetcher do
         end
 
         it 'writes the file and warns' do
-          file_fetcher.write_file_with_retries(filename: 'image111.tif', path: Pathname.new('/tmp/image111.tif'))
+          file_fetcher.write_file_with_retries(filename:, path:)
           expect(Preservation::Client).to have_received(:configure)
           expect(logger).to have_received(:warn).twice
 
           expect(objects_client).to have_received(:content).exactly(3).times # 2 failures, 3rd time is the charm
 
-          file1 = File.join(Settings.sdr.abbyy.local_ticket_path, 'bb222cc3333', 'image111.tif')
-          expect(File.read(file1)).to eq('Content for: image111.tif')
+          expect(File.read(file_path)).to eq("Content for: #{filename}")
         end
 
         context 'when retries are exhausted before the files show up on the preservation NFS mount' do
@@ -72,10 +76,10 @@ describe Dor::TextExtraction::FileFetcher do
           end
 
           it 'returns false, sends to HB, and logs an error' do
-            written = file_fetcher.write_file_with_retries(filename: 'image111.tif', path: Pathname.new('/tmp/image111.tif'))
+            written = file_fetcher.write_file_with_retries(filename:, path:)
             expect(written).to be(false)
 
-            context = { druid:, filename: 'image111.tif', path: '/tmp/image111.tif', cloud_endpoint: nil, max_tries: 3 }
+            context = { druid:, filename:, path: file_path, cloud_endpoint: nil, max_tries: 3 }
             expect(logger).to have_received(:error).with("Exceeded max_tries attempting to fetch file: #{context}")
             expect(Honeybadger).to have_received(:notify).with('Exceeded max_tries attempting to fetch file', context:)
             expect(file_fetcher).to have_received(:sleep).with(8) # should have hit max backoff time of 2^3 seconds
@@ -94,7 +98,7 @@ describe Dor::TextExtraction::FileFetcher do
 
       it 'fetches files from perservation and sends to cloud' do
         # TODO: add in actual expectations here when the method is implemented
-        file_fetcher.write_file_with_retries(filename: 'file1.mov', cloud_endpoint: 's3://some-bucket/file1.mov')
+        file_fetcher.write_file_with_retries(filename: 'file1.mov', cloud_endpoint:)
         expect(logger).to have_received(:info).once
         expect(objects_client).to have_received(:content).once
         expect(Preservation::Client).to have_received(:configure)
