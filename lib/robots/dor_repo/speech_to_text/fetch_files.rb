f5dd60c1517f9e260e6cd84eb69cabde11a6d1fe
Peter Mangiafico <peter@mangiafico.org>
2024-09-24 16:43:38 -0700

    simplify fetch files class a bit

--- lib/robots/dor_repo/speech_to_text/fetch_files.rb
+++ lib/robots/dor_repo/speech_to_text/fetch_files.rb
@@ -12,15 +12,11 @@ module Robots
         # available from LyberCore::Robot: druid, bare_druid, workflow_service, object_client, cocina_object, logger
         def perform_work
           sttable_filenames.each do |filename|
-            raise "Unable to fetch #{filename} for #{druid}" unless file_fetcher.write_file_with_retries(filename:, location: aws_provider.bucket.object(s3_pathname(filename)), max_tries: 3)
+            raise "Unable to fetch #{filename} for #{druid}" unless file_fetcher.write_file_with_retries(filename:, location: aws_provider.bucket.object(File.join(bare_druid, filename)), max_tries: 3)
           end
         end
 
         private
-
-        def s3_pathname(filename)
-          File.join(bare_druid, filename)
-        end
 
         def aws_provider
           @aws_provider ||= Dor::TextExtraction::AwsProvider.new(region: Settings.aws.region, access_key_id: Settings.aws.access_key_id, secret_access_key: Settings.aws.secret_access_key)
