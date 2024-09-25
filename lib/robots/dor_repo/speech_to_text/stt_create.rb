c8a7f6266295cf13fa089115c77a7060f2b0e46e
Peter Mangiafico <peter@mangiafico.org>
2024-09-25 16:12:17 -0700

    add tests

--- lib/robots/dor_repo/speech_to_text/stt_create.rb
+++ lib/robots/dor_repo/speech_to_text/stt_create.rb
@@ -12,10 +12,14 @@ module Robots
         # available from LyberCore::Robot: druid, bare_druid, workflow_service, object_client, cocina_object, logger
         def perform_work
           # start the caption creation by sending an SQS message to the caption creation service
+
           send_sqs_message
 
           # Leave this step running until the Whisper monitoring job marks it as complete
           LyberCore::ReturnState.new(status: :noop, note: 'Initiated SpeechToText.')
+        rescue StandardError => e
+          Honeybadger.notify('Problem sending SQS Message to AWS for SpeechToText', context: { druid:, error: e })
+          raise "Error sending SQS message: #{e.message}"
         end
 
         private
@@ -28,17 +32,26 @@ module Robots
           # Define the queue URL and message body
           queue_url = Settings.aws.sqs_queue_url
           message_body = {
-            druid: druid,
-            action: 'create_captions'
-          }.to_json
+            druid:
+          }.merge(whisper_options).to_json
 
           # Send the message to the SQS queue
           aws_provider.sqs.send_message({
-            queue_url: queue_url,
-            message_body: message_body
-          })
+                                          queue_url:,
+                                          message_body:
+                                        })
 
           logger.info("Sent SQS message for druid #{druid} to queue #{queue_url}")
+        end
+
+        def whisper_options
+          {
+            options: {
+              model: 'large',
+              max_line_count: 80,
+              beam_size: 10
+            }
+          }
         end
       end
     end
--- spec/robots/dor_repo/speech_to_text/stt_create_spec.rb
+++ spec/robots/dor_repo/speech_to_text/stt_create_spec.rb
@@ -7,8 +7,32 @@ describe Robots::DorRepo::SpeechToText::SttCreate do
 
   let(:druid) { 'druid:bb222cc3333' }
   let(:robot) { described_class.new }
+  let(:aws_client) { instance_double(Aws::SQS::Client) }
 
-  it 'runs the stt create robot' do
-    expect(perform.status).to eq 'noop'
+  before do
+    allow(Aws::SQS::Client).to receive(:new).and_return(aws_client)
+  end
+
+  context 'when the message is sent successfully' do
+    let(:message_body) { { druid:, options: { model: 'large', max_line_count: 80, beam_size: 10 } }.to_json }
+
+    before do
+      allow(aws_client).to receive(:send_message).with({ queue_url: Settings.aws.sqs_queue_url, message_body: }).and_return(true)
+    end
+
+    it 'sends SQS messages but does not complete the step' do
+      expect(perform.status).to eq 'noop'
+      expect(aws_client).to have_received(:send_message).with({ queue_url: Settings.aws.sqs_queue_url, message_body: }).once
+    end
+  end
+
+  context 'when the message is not sent successfully' do
+    before do
+      allow(aws_client).to receive(:send_message).and_raise(Aws::SQS::Errors::ServiceError.new(nil, 'blam'))
+    end
+
+    it 'raises an exception' do
+      expect { perform }.to raise_error(RuntimeError, 'Error sending SQS message: blam')
+    end
   end
 end
