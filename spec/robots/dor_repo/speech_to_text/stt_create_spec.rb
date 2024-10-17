75e1d0d90f1f75e9fd3ad7728286ec10e50ad126
Peter Mangiafico <peter@mangiafico.org>
2024-10-17 11:40:47 -0700

    adjust name of whisper model

--- spec/robots/dor_repo/speech_to_text/stt_create_spec.rb
+++ spec/robots/dor_repo/speech_to_text/stt_create_spec.rb
@@ -38,7 +38,7 @@ describe Robots::DorRepo::SpeechToText::SttCreate do
   end
 
   context 'when the message is sent successfully' do
-    let(:message_body) { { id: job_id, druid:, media:, options: { model: 'large-v3', beam_size: 10, writer: { max_line_width: 42, max_line_count: 1 } } }.to_json }
+    let(:message_body) { { id: job_id, druid:, media: }.merge(Settings.speech_to_text.whisper.to_h).to_json }
 
     before do
       allow(aws_client).to receive(:send_message).with({ queue_url: Settings.aws.speech_to_text.sqs_todo_queue_url, message_body: }).and_return(true)
