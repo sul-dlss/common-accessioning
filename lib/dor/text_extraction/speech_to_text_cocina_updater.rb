518d6599aab38837dcdb0b8b24ac61ec291ffba1
Peter Mangiafico <peter@mangiafico.org>
2024-11-08 14:02:10 -0800

    remove unncessary method

--- lib/dor/text_extraction/speech_to_text_cocina_updater.rb
+++ lib/dor/text_extraction/speech_to_text_cocina_updater.rb
@@ -15,13 +15,6 @@ module Dor
           'caption'
         end
       end
-
-      def resource_label(path)
-        extension = path.extname
-        raise "Unable to determine resource label for #{path}" unless extension == '.txt'
-
-        'Transcript'
-      end
     end
   end
 end
