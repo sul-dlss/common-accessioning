bcda74cdbec8baa182ab140f84efd987afc53219
Peter Mangiafico <peter@mangiafico.org>
2025-09-08 11:32:32 -0700

    do not fail if the number of page nodes does not match in the number of files listed in alto xml

--- spec/lib/dor/text_extraction/abbyy/split_alto_spec.rb
+++ spec/lib/dor/text_extraction/abbyy/split_alto_spec.rb
@@ -3,10 +3,14 @@
 require 'spec_helper'
 
 describe Dor::TextExtraction::Abbyy::SplitAlto do
-  subject(:results) { described_class.new(alto_path:) }
+  subject(:results) { described_class.new(alto_path:, logger:) }
+
+  let(:alto_path) { File.join(File.absolute_path('spec/fixtures/ocr'), "#{druid}_abbyy_alto.xml") }
 
   let(:druid) { 'bb222cc3333' }
-  let(:alto_path) { File.join(File.absolute_path('spec/fixtures/ocr'), "#{druid}_abbyy_alto.xml") }
+  let(:logger) { instance_double(Logger) }
+
+  before { allow(logger).to receive(:warn) }
 
   describe '.write_files' do
     let(:expected_files) { %w[bb222cc3333_00_0001.xml bb222cc3333_00_0002.xml bb222cc3333_00_0003.xml] }
@@ -20,6 +24,22 @@ describe Dor::TextExtraction::Abbyy::SplitAlto do
         actual_content = File.read(File.join(File.dirname(alto_path), file))
         expected_content = File.read(File.join(File.dirname(alto_path), "expected_#{file}"))
         expect(actual_content).to eq(expected_content)
+      end
+    end
+
+    context 'when there are fewer page filenames than Page nodes' do
+      let(:expected_files) { %w[bb222cc3333_00_0001.xml bb222cc3333_00_0002.xml] }
+      let(:third_missing_file) { 'bb222cc3333_00_0003.xml' }
+      let(:alto_path) { File.join(File.absolute_path('spec/fixtures/ocr'), "#{druid}_abbyy_alto_fewer_files.xml") }
+
+      after { expected_files.each { |file| FileUtils.rm_f(File.join(File.dirname(alto_path), file)) } }
+
+      it 'succeeeds and creates only first two XML files but logs a warning' do
+        expect(expected_files.all? { |file| File.exist?(File.join(File.dirname(alto_path), file)) }).to be false
+        expect(results.write_files).to be true
+        expect(expected_files.all? { |file| File.exist?(File.join(File.dirname(alto_path), file)) }).to be true
+        expect(File.exist?(File.join(File.dirname(alto_path), third_missing_file))).to be false
+        expect(logger).to have_received(:warn).with(/Page nodes exceed page filenames/)
       end
     end
   end
