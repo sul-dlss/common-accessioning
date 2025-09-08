a8e8fd966d14907df98bcca88124137c4fb09bf8
Peter Mangiafico <peter@mangiafico.org>
2025-09-08 13:54:44 -0700

    only run ocr_cleanup on specific servers

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ set :output, 'log/cron.log'
 set :environment_variable, 'ROBOT_ENVIRONMENT'
 
 # weekly cleanup of empty ABBYY folders in case cleanup step itself didn't do it correctly
-every :monday, at: '1am' do
+every :monday, at: '1am', roles: [:ocr_cleanup] do
   rake 'abbyy:cleanup[true]'
 end
