72c807829375d0ea19f5d533e9f3d247dbd256b4
Peter Mangiafico <peter@mangiafico.org>
2026-01-14 10:08:38 -0800

    extract first page for multi-page tiffs for OCR

--- lib/robots/dor_repo/ocr/fetch_files.rb
+++ lib/robots/dor_repo/ocr/fetch_files.rb
@@ -15,10 +15,26 @@ module Robots
             location = abbyy_path(filename)
             location.parent.mkpath unless location.parent.directory?
             raise "Unable to fetch #{filename} for #{druid}" unless file_fetcher.write_file_with_retries(filename:, location:, max_tries: 3)
+
+            replace_multipage_tiff(location)
           end
         end
 
         private
+
+        # if this is a multi-image TIFFs, we only use the first image for OCR, so extract first page and replace the tiff
+        # see https://github.com/sul-dlss/common-accessioning/issues/1589
+        def replace_multipage_tiff(location)
+          file = ::Assembly::Image.new(location.to_s)
+          return unless file.image? && file.multi_page?
+
+          # for an image that is multi-page, extract the first page and replace the original image with this extracted image
+          first_page_filepath = File.join(ocr.abbyy_input_path, "#{file.filename_without_ext}_first_page#{file.ext}")
+          result = file.extract_first_page(first_page_filepath)
+          raise "Failed to extract first page of multi-page TIFF #{file.filename}" unless result
+
+          FileUtils.mv(first_page_filepath, location.to_s, force: true) # overwrite the original multipage file with the extracted first page
+        end
 
         def abbyy_path(filename)
           # NOTE: if files of type "file" were allowed here we would have to
